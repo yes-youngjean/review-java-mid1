ebb339f6bc3001aa8dfb80976def2533bebae13a
YjNoh <yeszerojean@gmail.com>
2025-08-25 17:49:42 +0900

    Lesson: 예외처리가 필요한 이유2

---
+++ src/exception/ex1/NetworkServiceV1_1.java
@@ -0,0 +1,16 @@
+package exception.ex1;
+
+import exception.ex1.NetworkClientV1;
+
+public class NetworkServiceV1_1 {
+
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+        NetworkClientV1 client = new NetworkClientV1(address);
+        client.initError(data);
+
+        client.connect();
+        client.send(data);
+        client.disconnect();
+    }
+}
